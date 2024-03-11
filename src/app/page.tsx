fa75142b09e9206e82f15b9f923be638e59b42eb
Yogesh@8668 <yogeshpawar1512001@gmail.com>
2024-03-11 07:36:59 +0530

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+"use client"
+import React from "react";
+import Signup from "./Component/Signup";
+const Home = () => {
+    return (
+        <div className=" bg-violet-600 flex justify-center items-center h-screen">
+            <Signup />
+        </div>
+                
+    );
+};
+
+export default Home;
